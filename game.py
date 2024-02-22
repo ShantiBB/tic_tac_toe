7a32459f13174b3e73f617438031a4c538a29c06
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-02-23 07:37:01 +1000

    Improve game behavior and game ending scripts

--- game.py
+++ game.py
@@ -1,12 +1,100 @@
-from partsgame import Board
+from partsgame import Board, FieldIndexError, CellOccupiedError
 
 
 def main():
-    game = Board()  # Объект, который создает игровое поле
+    """Основная логика игры крестики-нолики."""
+    game = Board()
+    current_player = 'X'
+    running = True
     game.display()
-    game.make_move(1, 1, 'X')
-    print('Ход сделан!')
-    game.display()
+
+    while running:
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                # Координаты ячейки, которые задает пользователь.
+                row = int(input('Введите номер строки: ')) - 1
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: ')) - 1
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(f'Значение должно быть от 1 до {game.field_size}.')
+                continue
+            except ValueError:
+                print(f'Допускаются только цифры от 1 до {game.field_size}.')
+                print('Пожалуйста введите данные для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята.')
+                print('Введите другие координаты.')
+            except Exception:
+                print(f'Возникла ошибка: {Exception}')
+            else:
+                break
+
+        # Здесь координаты передаются на поле.
+        game.make_move(row, column, current_player)
+        game.display()
+        check_win(game)
+        is_board_full(game)
+        if check_win(game):
+            if current_player == 'X':
+                print('Выйграли крестики! Игра окончена.')
+                break
+            else:
+                print('Выйграли нолики! Игра окончена.')
+                break
+        if is_board_full(game) == 0:
+            print('Ничья! Игра окончена.')
+            break
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+def is_board_full(game):
+    """Цикл, который проверяет количество оставшихся свободных ячеек."""
+    count = 0
+    for line_field in game.board:
+        for point in line_field:
+            if point == ' ':
+                count += 1
+    if count == 0:
+        return 0
+
+
+def check_win(game):
+    """Цикл, который проверяет комбинации для победы."""
+    if (
+            # Комбинации для строк
+            game.board[0][0] == game.board[0][1] == game.board[0][2] == 'X'
+            or game.board[0][0] == game.board[0][1] == game.board[0][2] == 'O'
+            or game.board[1][0] == game.board[1][1] == game.board[1][2] == 'X'
+            or game.board[1][0] == game.board[1][1] == game.board[1][2] == 'O'
+            or game.board[2][0] == game.board[2][1] == game.board[2][2] == 'X'
+            or game.board[2][0] == game.board[2][1] == game.board[2][2] == 'O'
+
+            # Комбинации для столбцов
+            or game.board[0][0] == game.board[1][0] == game.board[2][0] == 'X'
+            or game.board[0][0] == game.board[1][0] == game.board[2][0] == 'O'
+            or game.board[0][1] == game.board[1][1] == game.board[2][1] == 'X'
+            or game.board[0][1] == game.board[1][1] == game.board[2][1] == 'O'
+            or game.board[0][2] == game.board[1][2] == game.board[2][2] == 'X'
+            or game.board[0][2] == game.board[1][2] == game.board[2][2] == 'O'
+
+            # Комбинации для диагоналей
+            or game.board[0][0] == game.board[1][1] == game.board[2][2] == 'X'
+            or game.board[0][0] == game.board[1][1] == game.board[2][2] == 'O'
+            or game.board[0][2] == game.board[1][1] == game.board[2][0] == 'X'
+            or game.board[0][2] == game.board[1][1] == game.board[2][0] == 'O'
+    ):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
--- partsgame/parts.py
+++ partsgame/parts.py
@@ -1,14 +1,19 @@
 class Board:
-    # Пустое поле 3х3
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+    """Класс, который описывает игровое поле."""
 
-    # Метод, который обрабатывает ход игрока
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
+    field_size = 3
+
+    def __init__(self):
+        self.board = [[' ' for _ in range(self.field_size)] for _ in range(self.field_size)]
+
+    def make_move(self, row, column, player):
+        self.board[row][column] = player
 
     # Метод, который отрисовывает игровое поле
     def display(self):
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+
+    def __str__(self):
+        return f'Объект игрового поля размером {self.field_size}x{self.field_size}'
