3a248be00d9722d5cd9898a47cbc7a2281c64bee
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-02-25 16:57:47 +1000

    Add win and draw combinations

--- game.py
+++ game.py
@@ -41,60 +41,17 @@ def main():
         # Здесь координаты передаются на поле.
         game.make_move(row, column, current_player)
         game.display()
-        check_win(game)
-        is_board_full(game)
-        if check_win(game):
+        if game.check_win(current_player):
             if current_player == 'X':
                 print('Выйграли крестики! Игра окончена.')
                 break
             else:
                 print('Выйграли нолики! Игра окончена.')
                 break
-        if is_board_full(game) == 0:
+        if game.is_board_full():
             print('Ничья! Игра окончена.')
             break
         current_player = 'O' if current_player == 'X' else 'X'
-
-
-def is_board_full(game):
-    """Цикл, который проверяет количество оставшихся свободных ячеек."""
-    count = 0
-    for line_field in game.board:
-        for point in line_field:
-            if point == ' ':
-                count += 1
-    if count == 0:
-        return 0
-
-
-def check_win(game):
-    """Цикл, который проверяет комбинации для победы."""
-    if (
-            # Комбинации для строк
-            game.board[0][0] == game.board[0][1] == game.board[0][2] == 'X'
-            or game.board[0][0] == game.board[0][1] == game.board[0][2] == 'O'
-            or game.board[1][0] == game.board[1][1] == game.board[1][2] == 'X'
-            or game.board[1][0] == game.board[1][1] == game.board[1][2] == 'O'
-            or game.board[2][0] == game.board[2][1] == game.board[2][2] == 'X'
-            or game.board[2][0] == game.board[2][1] == game.board[2][2] == 'O'
-
-            # Комбинации для столбцов
-            or game.board[0][0] == game.board[1][0] == game.board[2][0] == 'X'
-            or game.board[0][0] == game.board[1][0] == game.board[2][0] == 'O'
-            or game.board[0][1] == game.board[1][1] == game.board[2][1] == 'X'
-            or game.board[0][1] == game.board[1][1] == game.board[2][1] == 'O'
-            or game.board[0][2] == game.board[1][2] == game.board[2][2] == 'X'
-            or game.board[0][2] == game.board[1][2] == game.board[2][2] == 'O'
-
-            # Комбинации для диагоналей
-            or game.board[0][0] == game.board[1][1] == game.board[2][2] == 'X'
-            or game.board[0][0] == game.board[1][1] == game.board[2][2] == 'O'
-            or game.board[0][2] == game.board[1][1] == game.board[2][0] == 'X'
-            or game.board[0][2] == game.board[1][1] == game.board[2][0] == 'O'
-    ):
-        return True
-    else:
-        return False
 
 
 if __name__ == '__main__':
--- partsgame/parts.py
+++ partsgame/parts.py
@@ -15,5 +15,29 @@ class Board:
             print('|'.join(row))
             print('-' * 5)
 
+    def check_win(self, player):
+        """Цикл, который проверяет комбинации для победы."""
+        for i in range(self.field_size):
+            if (
+                    all([self.board[i][j] == player for j in range(self.field_size)])
+                    or all([self.board[j][i] == player for j in range(self.field_size)])
+            ):
+                return True
+        if (
+                # Комбинации для диагоналей
+                self.board[0][0] == self.board[1][1] == self.board[2][2] == player
+                or self.board[0][2] == self.board[1][1] == self.board[2][0] == player
+        ):
+            return True
+        return False
+
+    def is_board_full(self):
+        """Цикл, который проверяет количество оставшихся свободных ячеек."""
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                if self.board[i][j] == ' ':
+                    return False
+        return True
+
     def __str__(self):
         return f'Объект игрового поля размером {self.field_size}x{self.field_size}'
